526b932a4293204291d455b7e9a85375634d14a0
Avni Sharma <138052685+Avni15Sharma@users.noreply.github.com>
2024-09-10 10:25:49 +0000

    02_datatypes.js created

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high so avoid this
+
+console.log(3 + 3) //this is fine
+
+console.log("Awwwneee")
+
+
+let name = "avni"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value not type //ex: we receive a response from api, receiving null means we got no response ,bcoz 0 is still some response.
+// undefined => one of the data types  
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

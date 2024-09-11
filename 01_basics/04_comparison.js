a9b77ae05c96d4fb280f0f6e7ebcf6d0927bcbae
Avni Sharma <138052685+Avni15Sharma@users.noreply.github.com>
2024-09-11 08:53:27 +0000

    04_comparison.js created

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); 
+//comparison operator works on numbers ,and for that they convert operands to numbers
+// console.log(null == 0); 
+//== works differently than comparison operators
+// console.log(null >= 0); 
+//null is numerically 0
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+//undefined is not numerically 0
+// === 
+// console.log("2" == 2);
+
+// console.log("2" === 2);
+// the == operator does the type conversion of the operands before comparison, whereas the === operator compares the values as well as the data types of the operands.
+// console.log("string"==9);

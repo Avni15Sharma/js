5ff502dff97d21af57be003ee362240d432bd315
Avni Sharma <138052685+Avni15Sharma@users.noreply.github.com>
2024-09-11 08:57:54 +0000

    comparison modified

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,6 +8,7 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
+//null means empty not 0
 // console.log(null > 0); 
 //comparison operator works on numbers ,and for that they convert operands to numbers
 // console.log(null == 0); 
@@ -25,3 +26,5 @@
 // console.log("2" === 2);
 // the == operator does the type conversion of the operands before comparison, whereas the === operator compares the values as well as the data types of the operands.
 // console.log("string"==9);
+
+// JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime. Unlike languages such as Typescript or Java, you don't need to declare the data type of a variable explicitly.

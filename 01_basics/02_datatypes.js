cc88fbbe704fe6b3fc8ae13ed32014dadb63d103
Avni Sharma <138052685+Avni15Sharma@users.noreply.github.com>
2024-09-11 09:45:17 +0000

    summary added

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,18 +2,18 @@
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high so avoid this
+// console.log(3 
+//     +
+//      3) // code readability should be high so avoid this
 
-console.log(3 + 3) //this is fine
+// console.log(3 + 3) //this is fine
 
-console.log("Awwwneee")
+// console.log("Awwwneee")
 
 
 let name = "avni"
 let age = 21
-let isLoggedIn = false
+// let isLoggedIn = false
 let state;
 
 // number => 2 to power 53
@@ -27,5 +27,72 @@ let state;
 
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+
+// be aware!! summary ahead-------
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// Data Types: Primitive(call by value) and Non Primitive(call by reference) 
+let myName="Avni"
+let myAnotherName=myName
+// console.log(myName);
+// console.log(myAnotherName);
+
+// myAnotherName="AWWWNEEE"
+// console.log(myName);
+// console.log(myAnotherName);
+
+let userOne={
+    email: "abc@123.com",
+    id:67 
+}
+let userTwo=userOne
+// console.log(userOne.email);
+// console.log(userTwo.email);
+userTwo.email="main toh badal gyi"
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
+
+
+

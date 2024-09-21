b6562efd254d482048a34ca131a789f96b43b33e
Avni Sharma <138052685+Avni15Sharma@users.noreply.github.com>
2024-09-21 19:56:28 +0000

    five.js created

---
+++ 05_iterations.js/five.js
@@ -0,0 +1,72 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// // coding.forEach( function (val){
+// //     console.log(val);
+// // } )
+
+// // coding.forEach( (item) => {
+// //     console.log(item);
+// // } )
+
+// // function printMe(item){
+// //     console.log(item);
+// // }
+
+// // coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+// const myCoding = [
+//     {
+//         languageName: "javascript",
+//         languageFileName: "js"
+//     },
+//     {
+//         languageName: "java",
+//         languageFileName: "java"
+//     },
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
+// ]
+
+// myCoding.forEach( (item) => {
+    
+//     console.log(item.languageName);
+// } )
+//let's do it my way
+
+newarr=[7,5,3,2,1]
+// newarr.forEach(function print(item){
+//     console.log(item)
+// })
+// newarr.forEach(function (item){
+//     console.log(item)
+// })
+// newarr.forEach((item)=>{
+//     console.log(item)
+// })
+// function print(item){
+//     console.log(item)
+// }
+// newarr.forEach(print)
+// arr1=["ny","la","in"]
+// arr1.forEach((item,index,arr)=>{
+//     console.log(item,index,arr)
+// })
+// arrofObj=[{
+//     tradeid:865,
+//     dealername:"nattu kaka"
+// },{
+//     tradeid:234,
+//     dealername:"dibba"
+// },{
+//     tradeid:765,
+//     dealername:"babita"
+// }]
+// arrofObj.forEach((item)=>{
+//     console.log(item.dealername)
+// })

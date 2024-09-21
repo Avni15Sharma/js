0a5799d051fa71f89b968734ae8e3ea0dd6e0d05
Avni Sharma <138052685+Avni15Sharma@users.noreply.github.com>
2024-09-21 16:18:45 +0000

    two.js created

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,45 @@
+
+// let index = 0
+// // while (index <= 10) {
+// //     console.log(`Value of index is ${index}`);
+// //     index = index + 2
+// // }
+
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     //console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// let score = 11
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+//let's do it my way
+//while loop
+// let iter=11
+// while(iter>2){
+//     console.log(iter)
+//     iter--
+// }
+// arr=["oneplus","vivo","apple","google"]
+// index=0
+// while(index<arr.length){
+//     console.log(arr[index])
+//     index++
+// }
+// console.log(arr)
+// do{
+//     console.log("hehe")
+// }while(false)
+
+let j=0
+do{
+    console.log("nice",j)
+    j++
+}while(j<3)

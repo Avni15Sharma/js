b374e3ffca0f8c07deba1f8b8704e75ed5a3e0ef
Avni Sharma <138052685+Avni15Sharma@users.noreply.github.com>
2024-09-23 06:28:11 +0000

    modified

--- 05_iterations.js/reduce.js
+++ 05_iterations.js/reduce.js
@@ -44,7 +44,11 @@ schedule=[{
     hrs:2
 }]
 let hrsStudied=schedule.reduce((acc,curr)=>(acc+curr.hrs),0)
-console.log("hours I studied this week: " ,hrsStudied)
-let hrsEnjoyed=schedule.reduce((acc,curr)=>(acc-(24-curr.hrs)),2)
+// console.log("hours I studied this week: " ,hrsStudied)
+let hrsEnjoyed=schedule.reduce((acc,curr)=>{
+    console.log(`accumulator: ${acc} and current value: ${curr.hrs}`)
+    console.log(`current enjoyed: ${24-curr.hrs} `)
+    return acc+(24-curr.hrs)
+},2)
 console.log("hrsEnjoyed: ",hrsEnjoyed)
 //reduce element ki hr ek value pe operate krte hue sath sath accumulator ke sath operate krta hai aur end result/value return krta h

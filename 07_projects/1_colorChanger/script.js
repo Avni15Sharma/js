443aa9d91c24fc93869297129df159fbaee37126
Avni Sharma <138052685+Avni15Sharma@users.noreply.github.com>
2024-09-26 07:23:52 +0000

    modified

--- 07_projects/1_colorChanger/script.js
+++ 07_projects/1_colorChanger/script.js
@@ -1,25 +1,39 @@
-console.log("hitesh")
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
-
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
-    
-  });
-});
-
+const buttons=document.querySelectorAll(".button")
+const body=document.querySelector("body")
+text= document.querySelector(".heading1")
+const min=0;
+const max=3;
+function getNum(){
+    const num=Math.round(Math.random()*(max-min)+min)
+    return num
+}
+arr=["grey","white","lightcoral","lightblue"]
+buttons.forEach(function (button){
+    button.addEventListener("click",function (e){
+        body.style.backgroundColor=e.target.id;
+        let indToBeExcluded=-1;
+        switch(button.id){
+            case "grey":
+                indToBeExcluded=0
+                break;
+            case "white":
+                indToBeExcluded=1
+                break;
+            case "lightcoral":
+                indToBeExcluded=2
+                break;
+            case "lightblue":
+                indToBeExcluded=3
+                break;
+            default:
+                "invalid choice"
+                break;
+        }
+        let ind=getNum()
+        while(ind==indToBeExcluded){
+            ind=getNum()
+        }
+        console.log(arr[ind])
+        text.style.color=`${arr[ind]}`
+    });
+})
